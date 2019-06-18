fe831f7f6a6fce831d16d266c839bba93d27e715
LuKaas <lukaas.blu@gmail.com>
2019-06-18 11:17:03 +0100

    TDD : implement 'evaluate' spec

--- src/classes/calculator.spec.ts
+++ src/classes/calculator.spec.ts
@@ -11,7 +11,7 @@ describe('Calculator', () => {
     expect(calc.multiply(10, 13)).toEqual(130);
   });
 
-  xit('should evaluate 2 + 3 + 4 to 9', () => {
-    // @TODO implement this.
+  it('should evaluate 2 + 3 + 4 to 9', () => {
+    expect(calc.evaluate('2 + 3 + 4')).toEqual(9);
   });
 });
--- src/classes/calculator.ts
+++ src/classes/calculator.ts
@@ -1,7 +1,12 @@
 export default class Calculator {
+
   constructor() {}
 
   multiply(op1: number, op2: number): number {
     return op1 * op2;
+  }
+
+  evaluate(expression: string): number {
+    throw new Error('Evaluate method not implemented.');
   }
 }
