a78ef4a3de66c03ca251c42e14dbacebb5cdf7f9
LuKaas <lukaas.blu@gmail.com>
2019-06-18 11:06:44 +0100

    TDD : define 'evaluate' spec

--- src/classes/calculator.spec.ts
+++ src/classes/calculator.spec.ts
@@ -10,4 +10,8 @@ describe('Calculator', () => {
   it('should evaluate 10 * 13 to 130', () => {
     expect(calc.multiply(10, 13)).toEqual(130);
   });
+
+  xit('should evaluate 2 + 3 + 4 to 9', () => {
+    // @TODO implement this.
+  });
 });
